ce232836aee85e7bb5feccd064cb2fb90391940c
Haris Naseer <harisnaseer@aptech.com>
2024-11-27 10:30:27 +0500

    funtions and list

---
+++ lib/list.dart
@@ -0,0 +1,28 @@
+void main(){
+  List <int> odd=[1,3,5,7,9,11];
+  // print(odd);
+// odd.remove(3);
+// odd.removeAt(4);
+// print(odd);
+
+odd.add(13);
+odd.addAll([15,17,19,21]);
+// print(odd);
+
+// print(odd.length);
+
+// print(odd.isEmpty);//true/false
+// print(odd.isNotEmpty);//true/false
+
+print(odd.contains(18));//boolean
+print(odd.contains(17));//boolean
+
+print(odd.first);
+print(odd.last);
+print(odd.indexed);
+print(odd.elementAt(6));
+
+
+
+
+}
